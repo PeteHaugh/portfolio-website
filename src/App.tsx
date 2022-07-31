8a26be50e15479c1822458a65bf980edb1bfbe64
Peter Haugh <peter.ahaugh@gmail.com>
2022-07-31 11:53:56 +0100

    Refactored react router setup to improved UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout/Layout';
+import Synth from './components/Synth/Synth';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className='App'>
-      <Layout />
+    <div className='App' >
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/Synth" element={<Synth />} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/components/Synth/Synth.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+type Props = {}
+
+const Synth = (props: Props) => {
+  return (
+    <>
+        Synth mini project
+    </>
+  )
+}
+
+export default Synth
