fe7df3c95903c4d16582425f1cc6e531a061d58e
Peter Haugh <peter.ahaugh@gmail.com>
2022-09-19 11:24:06 +0100

    Updated board formatting

--- src/components/ConnectFour.tsx
+++ src/components/ConnectFour.tsx
@@ -1,7 +1,5 @@
 // @ts-nocheck
-import React, { useState, useReducer } from 'react'
-import { Axios } from '../api/ConnectFourAPI'
-import '../styles'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import axios from 'axios';
 
@@ -10,49 +8,81 @@ axios.defaults.baseURL = 'http://localhost:5000';
 const rows = 9;
 const columns = 7;
 
-const StyledGameBoard = styled.div`
-  display: grid;
-  grid-template-columns: repeat(6 , 1fr);
-  grid-template-rows: repeat(6, 1fr);
-  grid-gap: 10px;
-  height: 800px;
-  width: 800px;
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 3px solid lightblue;
+  height: 80vh;
+  width: 60vw;
+  margin: 0 auto;
 
+  justify-content: space-between;
+`;
+
+const StyledButtonRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    background-color: lightblue;
+    height: 50px;
+`;
+
+const StyledGameBoard = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background-color: lightblue;
+  height: 100%;
+  
   .cellYellow {
     border-radius: 50%;
     background-color: yellow;
+    grid-auto-flow: column;
   }
 
   .cellRed {
     border-radius: 50%;
     background-color: red;
+    grid-auto-flow: column;
+  }
+
+  .cellWhite {
+    background-color: white;
+    grid-auto-flow: column;
+    border-radius: 50%;
+    border: 2px solid lightblue;
   }
 `;
 
-// const StyledCell = styled.div`
-//   border-radius: 50%;
+const StyledColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: calc(60vw/6);
+  height: 100%;
+`;
 
-//   .red {
-//     background-color: red;
-//   }
+const StyledButton = styled.button`
+  width: calc(60vw/6);
+  max-height: 50px;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-//   .yellow {
-//     background-color: yellow;
-//   }
-// `;
-
-
-// try {
-//   response = await axios.get(`/move/${col}`);
-// } catch (error) {
-//   // handle error
-//   console.log(error);
-// }
-// return response.data
+const StyledCell = styled.div`
+  height: auto;
+  width: 100%;
+  aspect-ratio: 1/1;
+`;
 
 const ConnectFour = () => {
   const [board, setBoard] = useState([null, null])
   const columns = [...Array(6).keys()];
+
+  useEffect(() => {
+    handleReset()
+  }, [])
+  
 
   const handleMove = (column) => {
     axios.get(`/move/${column}`)
@@ -69,39 +99,50 @@ const ConnectFour = () => {
     <>
       <h1>ConnectFour!</h1>
 
-      
-      <StyledGameBoard>
-          {columns.map((column) => (
-                <button className='drop' key={column} name={column} onClick={() => handleMove(column + 1)}>{column+1}</button>
-          ))}
-          {console.log(board?.[0]?.[0])}
+      <StyledWrapper>
+        <StyledButtonRow>
+            {columns.map((column) => (
+                  <StyledButton  key={column} name={column} onClick={() => handleMove(column + 1)}>{column+1}</StyledButton>
+            ))}
 
+        </StyledButtonRow>
+        <StyledGameBoard>
+          <StyledColumn>
           {board && board?.[0]?.[0].map((cell, index) => (
-             <div className={cell === 'X' ? 'cellYellow' : 'cellRed'} key={index}>{cell}</div>
-           ))}
-           {board && board?.[0]?.[1].map((cell, index) => (
-             <div className={cell === 'X' ? 'cellYellow' : 'cellRed'} key={index}>{cell}</div>
-           ))}   
-           {board && board?.[0]?.[2].map((cell, index) => (
-             <div className={cell === 'X' ? 'cellYellow' : 'cellRed'} key={index}>{cell}</div>
-           ))}   
-          {board && board[1]}
-          
-
-
-      </StyledGameBoard>
-      <button onClick={() => handleReset()}>New Board</button>
-
-      {/* <table>
-        <tbody>
-          {gameState.board.map((row, i) => (
-
-            <Row key={i} row={row} play={play} />
-          ))}
-        </tbody>
-      </table> */}
-
-
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+          <StyledColumn>
+          {board && board?.[0]?.[1].map((cell, index) => (
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+          <StyledColumn>
+          {board && board?.[0]?.[2].map((cell, index) => (
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+          <StyledColumn>
+          {board && board?.[0]?.[3].map((cell, index) => (
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+          <StyledColumn>
+          {board && board?.[0]?.[4].map((cell, index) => (
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+          <StyledColumn>
+          {board && board?.[0]?.[5].map((cell, index) => (
+                <StyledCell className={cell === 'X' ? 'cellYellow' : cell === 'O' ? 'cellRed' : 'cellWhite'} key={index}>{cell}</StyledCell>
+              ))}  
+          </StyledColumn>
+        </StyledGameBoard>
+        <StyledButtonRow>
+        <button onClick={() => handleReset()}>New Board</button>
+        {board && board[1]}
+        </StyledButtonRow>
+      </StyledWrapper>
     </>
     
   )
